74d6c3efb22668dca3ae5976c461b7986e5fdae3
lucabrugnolini <luca_brugnolini@hotmail.it>
2016-02-23 21:38:00 +0100

    added bias corr

--- src/irf_ci_bootstrap.jl
+++ src/irf_ci_bootstrap.jl
@@ -1,14 +1,13 @@
 function irf_ci_bootstrap(V::VAR, H::Int64, nrep::Int64)
 
-  V.Y = V.Y'
-  (T,K) = size(V.Y)
+  (T,K) = size(V.Y')
   IRFrmat = zeros(nrep, K^2*(H+1))
   u = V.ϵ*sqrt((T-V.p)/(T-V.p-K*V.p-1))          # rescaling residual (Stine, JASA 1987)
   for j in 1:nrep
     # Draw block of initial pre-sample values
     yr = zeros(K,T)                                       # bootstrap data
     pos = trunc(Integer, (rand()*(T-V.p+1)))+1            # position of initial draw
-    yr[:,1:V.p] = V.Y[pos:pos+V.p-1,:]'                   # drawing pre-sample obs
+    yr[:,1:V.p] = V.Y'[pos:pos+V.p-1,:]'                   # drawing pre-sample obs
     # Draw innovations
     ur = zeros(K,T)                             # bootstrap innovations
     index = trunc(Integer, (rand(T-V.p)*(T-V.p))+1)         # index for innovation draws
@@ -26,17 +25,13 @@ function irf_ci_bootstrap(V::VAR, H::Int64, nrep::Int64)
     end
     #pr = V.p # also using lag length selection
     Vr = VAR(yr,V.p,true)
-    #(A1r,SIGMA1r) = VAR(yr,pr, i=true)
-    Ar = get_VAR1_rep(Vr)
-    #Ar = [A1r[:,2:end]; [eye(K*(pr-1)) zeros(K*(pr-1),K)]]   # Slope parameter estimates in companion form
-    SIGMAr = [Vr.Σ zeros(K,K*Vr.p-K); zeros(K*Vr.p-K,K*Vr.p)]   # variance of residuals in companion form
     # Bias correction: if the largest root of the companion matrix
     # is less than 1, do BIAS correction
     #if ~ any(abs(eigvals(Ar)).>=1)
     #  Ar = asybc(Ar,SIGMAr,T,K,Vr.p) #############################CHECK
     #end
-    Ar = real(Ar)
-    IRFr = irf(Ar[1:K,1:K*Vr.p], SIGMAr[1:K,1:K], H) ##############PROBLEM HERE
+    #Ar = real(Ar)
+    IRFr = irf(Vr, H, true) ##############PROBLEM HERE
     IRFrmat[j,:] = vec(IRFr')'
   end                              # end bootstrap
   # Calculate 95 perccent interval endpoints
--- src/var.jl
+++ src/var.jl
@@ -35,14 +35,18 @@ include("t_test.jl")
 include("get_VAR1_rep.jl")
 include("get_VAR_lag_length.jl")
 include("irf.jl")
-export VAR, t_test, get_VAR1_rep, get_VAR_lag_length, irf
+include("irf_ci_bootstrap.jl")
+include("bias_correction.jl")
+export VAR, t_test, get_VAR1_rep, get_VAR_lag_length, irf, irf_ci_bootstrap, bias_correction
 end # end of the module
 
 # Example VAR K=4 T=100
 using VARs
 data = rand(100,4)
-p = get_VAR_lag_length(data,12,"aic",true) # select lag-length using aic, bic, hq, aicc
-V = VAR(data,p,true) # fit a VAR model to data
+#p = get_VAR_lag_length(data,12,"aic",true) # select lag-length using aic, bic, hq, aicc
+V = VAR(data,2,true) # fit a VAR model to data
 T = t_test(V) # test coefficient significance
 get_VAR1_rep(V) # get companion form
 irf(V,20,true) # get impulse response function (reduce_form or cholesky)
+bias_correction(V)
+irf_ci_bootstrap(V,10,1000)
