cfa1e735466f1dec8a4f9380aa72d77aa21ff167
Luca Brugnolini <luca.brugnolini@gmail.com>
2018-07-06 14:11:18 +0200

    added bvar

--- src/VectorAutoregressions.jl
+++ src/VectorAutoregressions.jl
@@ -1,4 +1,9 @@
+# This package is a work in progress for the estimation and identification of Vector Autoregressive (VAR) models.
+# Credits:
+# Kilian and Kim 2011, Cremfi codes
+
 module VectorAutoregressions
+using Parameters, GrowableArrays
 
 type Intercept end
 
@@ -16,6 +21,281 @@ end
 function VAR(y::Array,p::Int64,i::Bool)
     i == false ? ((Y,X,β,ϵ,Σ,p) = fit(y,p)) : ((Y,X,β,ϵ,Σ,p) = fit(y,p,Intercept()))
     return VAR(Y,X,β,ϵ,Σ,p,Intercept())
+end
+
+function bVAR(y::Array,p::Int64,i::Bool,reps::Int64,burn::Int64,max_try::Int64,prior::Hyperparameter)
+    i == false ? ((Y,X,β,ϵ,Σ,p) = fit_bvar(y,prior)) : ((Y,X,β,ϵ,Σ,p) = fit_bvar(y,Intercept()))
+    return VAR(Y,X,β,ϵ,Σ,p,Intercept())
+end
+
+function get_prior(y::Array,p::Int64 = 1)
+    T,K = size(y) 
+    μ = mean(y,1)'
+    σ = Array{Float64}(0)
+    δ = Array{Float64}(0)
+    e0 = Array{Float64}(T-p,0)
+    for i = 1:K
+        ytemp = y[:,i]
+        xtemp = [lagmatrix(ytemp,p) ones(length(ytemp)-p)]
+        ytemp = ytemp[1+p:end,:]
+        btemp=xtemp\ytemp
+        etemp=ytemp-xtemp*btemp
+        stemp=etemp'*etemp/length(ytemp)
+        if abs(btemp[1]) > 1
+            btemp[1] = 1
+        end
+        σ = vcat(σ,stemp)
+        δ = vcat(δ,btemp[1])
+        e0 = hcat(e0, etemp)
+    end
+    return μ, σ, δ, e0 
+end
+
+function create_dummies(λ::Float64,τ::Float64,δ::Array,ε::Float64,p::Int64,μ::Array,σ::Array,K::Int64)
+    if typeof(σ) == Array{Float64,2}
+        σ = σ[:,1]
+    end
+    if typeof(μ) == Array{Float64,2}
+        μ = μ[:,1]
+    end
+    if λ > 0
+        if ε > 0
+            yd1 = [diagm(σ.*δ)./λ;
+            zeros(K*(p-1),K);
+            diagm(σ);
+            zeros(1,K)]
+            
+            jp = diagm(1:p)
+            xd1 = [hcat(kron(jp,diagm(σ)./λ), zeros((K*p),1));
+            zeros(K,(K*p)+1);
+            hcat(zeros(1,K*p), ε)]
+        else
+            yd1 = [diagm(σ.*δ)./λ;
+            zeros(K*(p-1),K);
+            diagm(σ)]
+            
+            jp = diagm(1:p)
+            xd1 = [kron(jp,diagm(σ)./λ);
+            zeros(K,(K*p))] 
+        end
+    end
+    if τ > 0
+        if ε > 0
+            yd2 = diagm(δ.*μ)./τ
+            xd2 = [kron(ones(1,p),yd2) zeros(K,1)]
+        else
+            yd2 = diagm(δ.*μ)./τ
+            xd2 = [kron(ones(1,p),yd2)]  
+        end
+    end
+    y = vcat(yd1,yd2)
+    x = vcat(xd1,xd2)
+    return y,x
+end
+
+function sum_loggamma(K::Int64,v::Int64)
+    out = 0
+    for i = 1:K
+        out = out + lgamma((v+1-i)/2)
+    end
+    return out
+end
+
+function mgamln(K::Int64,v::Int64)
+    constant = (K*(K-1)/4)*log(pi)
+    term2 = sum_loggamma(K,v)
+    out = constant + term2
+    return out
+end
+
+function max_lik_var(y::Array,x::Array,yd::Array,xd::Array)
+    T,K = size(y)
+    v = size(yd,1)
+    y1 = vcat(y,yd)
+    x1 = vcat(x,xd)
+    #prior moments
+    xx0 = xd'*xd
+    invxx0 = pinv(xx0) 
+    b0 = invxx0*xd'*yd
+    v0 = size(yd,1)
+    e0 = yd-xd*b0
+    sigma0 = e0'*e0
+    #posterior moments
+    xx1 = x1'*x1
+    invxx = pinv(xx1)
+    b = invxx*x1'*y1
+    v1 = v0+T 
+    e = y1-x1*b 
+    sigma1 = e'*e
+    
+    PP = inv(eye(T)+x*invxx0*x') 
+    QQ = sigma0
+    
+    lngam_ratio = mgamln(K,v0)-mgamln(K,v1)
+    py = -(lngam_ratio+(T*K/2)*log(pi))+0.5*K*log(det(PP))+(v0/2)*log(det(QQ))-(v1/2)*log(det(QQ+(y-x*b0)'*PP*(y-x*b0)))
+    out = py
+    return out
+end
+
+function get_opt_lag_prior(y::Array,p::Int64,λ::Range{Float64},τ::Range{Float64},δ::Array,ε::Float64,μ::Array,σ::Array)
+    K  =  size(y,2)
+    outmlik = zeros(length(p),length(λ))
+    tableL = zeros(length(p),length(λ))
+    tableP = copy(tableL)
+    tableD = copy(tableL)
+    for i = 1:length(p)
+        L = p[i]
+        Y = copy(y)
+        X = lagmatrix(y,L)
+        X = hcat(X, ones(size(X,1)))
+        Y = Y[L+1:end,:]
+        for j = 1:length(λ)
+            yd,xd  =  create_dummies(λ[j],τ[j],δ,ε,L,μ,σ,K)
+            # get marginal likelihood
+            mlik = max_lik_var(Y,X,yd,xd)
+            outmlik[i,j] = mlik
+            tableL[i,j] = L
+            tableP[i,j] = λ[j]
+            tableD[i,j] = τ[j]
+        end
+    end
+    id = outmlik .== maximum(maximum(outmlik))
+    optL = tableL[id]
+    optP = tableP[id]
+    optD = tableD[id]
+    return optL, optP, optD
+end
+
+function get_coef(β::Array,sigma::Array,ixx::Array,max_try::Int64,K::Int64,p::Int64)
+    problem = 0
+    vstar = kron(sigma,ixx)
+    check = -1
+    num_try = 1
+    control_stability = 0
+    mβ = Array{}
+    while check < 0 && num_try < max_try
+        mβ = β + (randn(1,K*(K*p+1))*chol(Hermitian(vstar)))'
+        
+        control_stability = check_stability(mβ,K,p)
+        if control_stability == 0
+            check = 10
+        else
+            num_try += 1
+        end
+    end
+    if control_stability > 0
+        problem = 1
+    end
+    return mβ,problem
+end
+
+function check_stability(β::Array,K::Int64,p::Int64)
+    Kp = K*p 
+    K_p1 = K*(p-1)
+    mCoef = zeros(Kp,Kp)
+    mCoef[K+1:Kp,1:K_p1] = eye(K_p1,K_p1)
+    
+    temp = reshape(β,Kp+1,K)
+    temp = view(temp,1:Kp,1:K)'
+    mCoef[1:K,1:Kp] = temp
+    ee = maximum(abs.(eigvals(mCoef)))
+    is_stable = ee >= 1
+    return is_stable
+end
+
+function iwpq(v::Int64,ixpx::Array)
+    k = size(ixpx,1)
+    z = zeros(v,k)
+    mu = zeros(k,1)
+    for i = 1:v
+        z[i,:]=(chol(Hermitian(ixpx))'*randn(k,1))'
+    end
+    out = inv(z'*z)
+    return out
+end    
+
+function get_forecast(K::Int64, H::Int64, T::Int64, p::Int64, Y::SubArray, β::Array, sigma::Array) 
+    # -------------------------------------------------------------------------
+    # get_paths:
+    # generates a matrix of simulated paths for Y for given parameters and
+    # general set-up (lags, horizon, etc.)
+    # -------------------------------------------------------------------------
+    # Draw K(0,1) innovations for variance and mean equation:
+    csigma = chol(Hermitian(sigma))
+    uu = randn(H+p,K)
+    # Note we only need H*K innovations, but adding an extra p draws makes 
+    # the indexing in the loop below a bit cleaner.
+    #compute forecast
+    yhat = zeros(H+p,K)
+    yhat[1:p,:] = Y[T-p+1:T,:]
+    for fi = p+1:H+p
+        xhat= Array{Float64}(0) 
+        for ji = 1:p
+            xhat = vcat(xhat,yhat[fi-ji,:])
+        end
+        xhat = vcat(xhat,1)
+        yhat[fi,:] = xhat'*reshape(β,K*p+1,K) + uu[fi,:]'*csigma
+    end
+    return yhat
+end
+
+function gibbs!(Y::Array,yd::Array,xd::Array,p::Int64,prior::Hyperparameter,mForecast::Array)
+    @unpack λ ,τ ,ε ,H ,reps ,burnin ,max_try ,update = prior 
+    x = lagmatrix(Y,p)
+    T = size(x,1)
+    K = size(Y,2)
+    iT = size(yd,1)
+    x = hcat(x, ones(T))
+    y = view(Y,p+1:T+p,:)
+    
+    y0 = [y; yd]
+    x0 = [x; xd]
+    # conditional mean of the VAR coefficients
+    β0_fix = vec(x0\y0) # ols on the appended data
+    β0 = vec(x0\y0) # ols on the appended data
+    xx = x0'*x0
+    ixx = xx\eye(size(xx,2))  # inv(x0'x0) to be used later in the Gibbs sampling lgorithm
+    σ = eye(K) # starting value for sigma
+    igibbs = 1
+    jgibbs = 0
+    
+    while jgibbs < reps-burnin
+        # Display progress:
+        if mod(igibbs,update)==0 
+            println("Replication $igibbs of $reps. Lag $p Prior tightness $λ")
+        end
+        # #step 1: Sample VAR coefficients
+        β1, problem = get_coef(β0_fix,σ,ixx,max_try,K,p)
+        if problem == 1
+            β1 = β0
+        else
+            β0 = β1
+        end
+        #draw covariance
+        e = y0-x0*reshape(β1,K*p+1,K)
+        scale = e'*e
+        v = T+iT
+        inv_scale = inv(scale)
+        σ = iwpq(v,inv_scale)
+        if igibbs > burnin && problem == 0
+            jgibbs += 1
+            yhat = get_forecast(K, H+1, T, p, y, β1,σ)
+            mForecast[jgibbs,:,:] = view(yhat,p+1:H+p,:)
+        end
+        igibbs += 1
+    end
+    return mForecast
+end
+
+function fit_bvar(y::Array,prior::Hyperparameter)
+    @unpack λ ,τ ,ε ,p ,H ,reps ,burnin ,max_try ,update = prior 
+    T,K = size(y)
+    μ, σ, δ, e0  = get_prior(y)
+    p_star,λ_star,τ_star = get_opt_lag_prior(y,p,λ,τ,δ,ε,μ,σ)
+    yd,xd = create_dummies(λ_star[1],τ_star[1],δ,ε,Int(p_star[1]),μ,σ,K)
+    mForecast = zeros(reps-burnin,H,K) 
+    gibbs!(y,yd,xd,Int(p_star[1]),prior,mForecast)
+    return mForecast
 end
 
 abstract type CI end
@@ -64,6 +344,32 @@ function IRFs_b(V::VAR,H::Int64,nrep::Int64,i::Bool)
     return IRFs(mIRF,CI)
 end
 
+function fit(y::Array,p::Int64)
+    (T,K) = size(y)
+    T < K && error("error: there are more covariates than observation")
+    X = y
+    y = transpose(y)
+    Y = y[:,p+1:T]
+    X = lagmatrix(X,p)'
+    β = (Y*X')/(X*X')
+    ϵ = Y - β*X
+    Σ = ϵ*ϵ'/(T-p-p*K-1)
+    return Y,X,β,ϵ,Σ,p
+end
+
+function fit(y::Array,p::Int64,inter::Intercept)
+    (T,K) = size(y)
+    T < K && error("error: there are more covariates than observation")
+    X = y
+    y = transpose(y)
+    Y = y[:,p+1:T]
+    X = lagmatrix(X,p,inter)'
+    β = (Y*X')/(X*X')
+    ϵ = Y - β*X
+    Σ = ϵ*ϵ'/(T-p-p*K-1)
+    return Y,X,β,ϵ,Σ,p
+end
+
 function lagmatrix{F}(x::Array{F},p::Int64,inter::Intercept)
     sk = 1
     T, K = size(x)
@@ -99,42 +405,32 @@ function lagmatrix{F}(x::Array{F},p::Int64)
     return X[:,2:end]
 end
 
-function fit(y::Array,p::Int64)
-    (T,K) = size(y)
-    T < K && error("error: there are more covariates than observation")
-    X = y
-    y = transpose(y)
-    Y = y[:,p+1:T]
-    X = lagmatrix(X,p)'
-    β = (Y*X')/(X*X')
-    ϵ = Y - β*X
-    Σ = ϵ*ϵ'/(T-p-p*K-1)
-    return Y,X,β,ϵ,Σ,p
+function lagmatrix{F}(x::Vector{F},p::Int64)
+    sk = 1
+    T = length(x)
+    K = 1
+    k    = K*p+1
+    idx  = repmat(1:K, p)
+    X    = Array{F}(T-p, k)
+    # building X (t-1:t-p) allocating data from D matrix - avoid checking bounds
+    for j = 1+sk:(sk+K*p)
+        for i = 1:(T-p)
+            lg = round(Int, ceil((j-sk)/K)) - 1 # create index [0 0 1 1 2 2 ...etc]
+            @inbounds X[i, j] = x[i+p-1-lg, idx[j-sk]]
+        end
+    end
+    return X[:,2:end]
 end
 
-function fit(y::Array,p::Int64,inter::Intercept)
-    (T,K) = size(y)
-    T < K && error("error: there are more covariates than observation")
-    X = y
-    y = transpose(y)
-    Y = y[:,p+1:T]
-    X = lagmatrix(X,p,inter)'
-    β = (Y*X')/(X*X')
-    ϵ = Y - β*X
-    Σ = ϵ*ϵ'/(T-p-p*K-1)
-    return Y,X,β,ϵ,Σ,p
-end
-
-
-# returns Magnus and Neudecker's commutation matrix of dimensions n by m
 function commutation(n::Int64, m::Int64)
+    # returns Magnus and Neudecker's commutation matrix of dimensions n by m
     k = reshape(kron(vec(eye(n)), eye(m)), n*m, n*m)
     return k
 end
 
-# Returns Magnus and Neudecker's duplication matrix of size n
-# VERY AMBIGUOUS FUNC
 function duplication(n::Int64)
+    # Returns Magnus and Neudecker's duplication matrix of size n
+    # VERY AMBIGUOUS FUNC
     a = tril(ones(n,n))::Array{Float64}
     i = find(a)::Vector{Int64}
     a[i] = 1:size(i,1)
@@ -148,9 +444,9 @@ function duplication(n::Int64)
     return d
 end
 
-# elimat(m) returns the elimination matrix Lm
-# The elimination matrix Lm is for any matrix F, Vech(F)=Lm Vec(F)
 function elimat(m::Int64)
+    # elimat(m) returns the elimination matrix Lm
+    # The elimination matrix Lm is for any matrix F, Vech(F)=Lm Vec(F)
     A = eye(m^2)
     L = A[1:m,:]
     for n in 2:m
@@ -480,8 +776,8 @@ function get_bias(mSigma::Array,mSigma_y::Array,B::Array,I::Array,mSum_eigen::Ar
     return mBias = mSigma*(inv(I - B) + B/(I-B^2) + mSum_eigen)/(mSigma_y)
 end
 
-# Bias-correction Pope (1990)
 function bias_correction(V::VAR,mVar1::Array)
+    # Bias-correction Pope (1990)
     K,T = size(V.Y)::Tuple{Int64,Int64}
     mSigma = get_companion_vcv(V)
     mSigma_y = get_sigma_y(V,mVar1,mSigma)
---
+++ test/runtest_bvar.jl
@@ -0,0 +1,23 @@
+# Example - BVAR
+using VectorAutoregressions, Plots
+plotly()
+
+y = CSV.read(joinpath(Pkg.dir("VectorAutoregressions.jl"),"test","bvar_test.csv"), header = false)
+y = convert(Array,y[:,1:3])
+const λ = 0.1:0.1:1
+const τ = 10*(0.1:0.1:1)
+const ε = 0.0001
+const p = 4
+const H = 10
+const reps = 10000
+const burnin = 1000
+const max_try = 1000
+const update = 1000
+prior = Hyperparameter(λ,τ,ε,p,H,reps,burnin,max_try,update)
+
+mForecast = fit_bvar(y,prior)
+plot(layout = grid(1,3))
+for j in 1:3
+plot(1:size(y,1), y[:,j])
+plot!(size(y,1)+1:size(y,1)+H, [[percentile(mForecast[:,i,j],16) for i in 1:H] median(mForecast[:,:,j],1)'  [percentile(mForecast[:,i,j],84) for i in 1:H]])
+end
