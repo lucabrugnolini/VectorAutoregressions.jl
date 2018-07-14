a0478a52676c6412ce90d292b7509aa65e0bb249
Luca Brugnolini <luca.brugnolini@gmail.com>
2018-07-14 13:20:20 +0200

    cleaning old files

--- src/commutation.jl
+++
@@ -1,5 +0,0 @@
-# returns Magnus and Neudecker's commutation matrix of dimensions n by m
-function commutation(n::Int64, m::Int64)
-  k = reshape(kron(vec(eye(n)), eye(m)), n*m, n*m)
-  return k
-end
--- src/get_VAR1_rep.jl
+++
@@ -1,9 +0,0 @@
-function get_VAR1_rep(V::VAR)
-    K = size(V.Σ,1)
-    if V.i == true
-       v = [V.β[:,1]; zeros(K*(V.p-1),1)]; B = [V.β[:,2:end,]; eye(K*(V.p-1)) zeros(K*(V.p-1),K)]
-    else B = [V.β; eye(K*(V.p-1)) zeros(K*(V.p-1),K)]
-    end
-    #V.i == true ? (return B, v) : (return B)
-    return B
-end
