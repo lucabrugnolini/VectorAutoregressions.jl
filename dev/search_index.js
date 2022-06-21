5ddcd32b3c0381659e70cc5e76ffdde4140edb2f
Documenter.jl <documenter@juliadocs.github.io>
2022-06-21 18:31:16 +0000

    build based on 1863013

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VectorAutoregressions.jl","page":"VectorAutoregressions.jl","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"","page":"VectorAutoregressions.jl","title":"VectorAutoregressions.jl","text":"Documentation for VectorAutoregressions.jl","category":"page"}]
+}
